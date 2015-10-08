6383d700627ecd527da6b201b631dffe542295e4
markatgigabyte <mark@gigabyte.com>
2015-10-09 00:43:11 +0800

    Update InstallSmartRecovery.bat

--- WIn10Recovery/InstallSmartRecovery.bat
+++ WIn10Recovery/InstallSmartRecovery.bat
@@ -1,10 +1,13 @@
 
 set TARGETDISK=0
+set RECOVERYPARTIONSIZE=15000
 set TARGETOSMODE="UEFI"
+
 set USBDISKVOL=X:
 
 rem Step1: Create RecoveryImage Partition
-batch\BatchSubstitute.bat "$TARGETDISK" %TARGETDISK% CreateRecoveryPartitions-%TARGETOSMODE%.txt > CreateRecoveryPartitions.scp
+batch\BatchSubstitute.bat "$TARGETDISK$" %TARGETDISK% CreateRecoveryPartitions-%TARGETOSMODE%.txt > CreateRecoveryPartitions.scp
+batch\BatchSubstitute.bat "$RECOVERYPARTIONSIZE$" %RECOVERYPARTIONSIZE% CreateRecoveryPartitions.scp > CreateRecoveryPartitions.scp
 diskpart /s CreateRecoveryPartitions.scp
 mkdir r:\RecoveryImage
 copy %USBDISKVOL%\RecoveryImage\Install.wim r:\RecoveryImage
@@ -20,7 +23,8 @@ copy  %USBDISKVOL%\RecoveryImage\OEM\Resetconfig-%TARGETOSMODE%.xml r:\RecoveryI
 copy %USBDISKVOL%\RecoveryImage\OEM\\ResetPartitions-BIOS.txt r:\RecoveryImage\OEM
 copy %USBDISKVOL%\RecoveryImage\OEM\\ResetPartitions-UEFI.txt r:\RecoveryImage\OEM
 
-
-
+OEMResetBtnSupport.cmd
+HWRecoveryBtnSupport.cmd
+CustomToolBtnSupport.cmd
 
 
