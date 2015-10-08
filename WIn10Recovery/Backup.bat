060ac67f8cbc483ba4f7171e4af5d4ea6682bcc7
markatgigabyte <mark@gigabyte.com>
2015-10-08 18:47:46 +0800

    Update Backup.bat

--- WIn10Recovery/Backup.bat
+++ WIn10Recovery/Backup.bat
@@ -1,23 +1,31 @@
 
 set TARGETDISK=0
 set TARGETOSMODE="UEFI"
+set USBDISKVOL=X:
 
 rem Step1: Create RecoveryImage Partition
 batch\BatchSubstitute.bat "$TARGETDISK" %TARGETDISK% CreateRecoveryPartitions-%TARGETOSMODE%.txt > CreateRecoveryPartitions.scp
 diskpart /s CreateRecoveryPartitions.scp
 mkdir r:\RecoveryImage
-rem Step2: Create install.wim
+copy %USBDISKVOL%\RecoveryImage\Install.wim r:\RecoveryImage
+mkdir r:\RecoveryImage\OEM
+copy %USBDISKVOL%\RecoveryImage\OEM\\BasicReset_AfterImageApply.cmd r:\RecoveryImage\OEM
+copy %USBDISKVOL%\RecoveryImage\OEM\\BasicReset_BeforeImageApply.cmd r:\RecoveryImage\OEM
+copy %USBDISKVOL%\RecoveryImage\OEM\\FactoryReset_AfterDiskFormat.cmd.cmd r:\RecoveryImage\OEM
+copy %USBDISKVOL%\RecoveryImage\OEM\\FactoryReset_AfterImageApply.cmd r:\RecoveryImage\OEM
+rem OEM\B
+asicReset_AfterImageApply.cmd,
+rem OEM\BasicReset_BeforeImageApply.cmd
+rem OEM\FactoryReset_AfterDiskFormat.cmd
+rem OEM\FactoryReset_AfterImageApply.cmd
 
 rem Step3: create Resetconfig.xml 
-batch\BatchSubstitute.bat "$TARGETDISK" %TARGETDISK% Resetconfig-%TARGETOSMODE%.xml > Resetconfig.xml
-batch\BatchSubstitute.bat "$TARGETDISK" %TARGETDISK% Resetconfig-%TARGETOSMODE%.xml > Resetconfig.xml
+copy  %USBDISKVOL%\RecoveryImage\OEM\Resetconfig-%TARGETOSMODE%.xml r:\RecoveryImage\Resetconfig.xml
 
-rem BasicReset_AfterImageApply.cmd,
-rem BasicReset_BeforeImageApply.cmd
-rem FactoryReset_AfterDiskFormat.cmd
-rem FactoryReset_AfterImageApply.cmd
 
-rem Ste4: create ResetPartitions-BIOS.txt
+
+rem ResetPartitions-BIOS.txt
+rem ResetPartitions-UEFI.txt
 
 
 
