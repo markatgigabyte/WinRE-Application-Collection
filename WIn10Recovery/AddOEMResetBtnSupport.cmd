6dfcf6be4be851cabf4af69f3bfb3691366cf0e8
markatgigabyte <mark@gigabyte.com>
2015-10-09 02:01:33 +0800

    Update AddOEMResetBtnSupport.cmd

--- WIn10Recovery/AddOEMResetBtnSupport.cmd
+++ WIn10Recovery/AddOEMResetBtnSupport.cmd
@@ -1,8 +1,12 @@
 rem AddOEMResetBtnSupport.cmd
-mkdir r:\RecoveryImage\OEM
-copy %USBDISKVOL%\RecoveryImage\OEM\BasicReset_AfterImageApply.cmd r:\RecoveryImage\OEM
-copy %USBDISKVOL%\RecoveryImage\OEM\BasicReset_BeforeImageApply.cmd r:\RecoveryImage\OEM
-copy %USBDISKVOL%\RecoveryImage\OEM\FactoryReset_AfterDiskFormat.cmd r:\RecoveryImage\OEM
-copy %USBDISKVOL%\RecoveryImage\OEM\FactoryReset_AfterImageApply.cmd r:\RecoveryImage\OEM
+mkdir -p r:\RecoveryImage\OEM
 copy %USBDISKVOL%\RecoveryImage\OEM\Resetconfig-%TARGETOSMODE%.xml r:\RecoveryImage\Resetconfig.xml
+copy %USBDISKVOL%\RecoveryImage\OEM\BasicReset_AfterImageApply.cmd r:\RecoveryImage\OEM
+copy %USBDISKVOL%\RecoveryImage\OEM\SaveLogFiles.cmd r:\RecoveryImage\OEM
+copy %USBDISKVOL%\RecoveryImage\OEM\BasicReset_BeforeImageApply.cmd r:\RecoveryImage\OEM
+copy %USBDISKVOL%\RecoveryImage\OEM\RetrieveLogFiles.cmd r:\RecoveryImage\OEM
+copy %USBDISKVOL%\RecoveryImage\OEM\FactoryReset_AfterDiskFormat.cmd r:\RecoveryImage\OEM
+copy %USBDISKVOL%\RecoveryImage\OEM\CheckPartitions.cmd r:\RecoveryImage\OEM
+copy %USBDISKVOL%\RecoveryImage\OEM\FactoryReset_AfterImageApply.cmd r:\RecoveryImage\OEM
+copy %USBDISKVOL%\RecoveryImage\OEM\InstallApps.cmd r:\RecoveryImage\OEM
 copy %USBDISKVOL%\RecoveryImage\OEM\ResetPartitions-%TARGETOSMODE%.txt r:\RecoveryImage\OEM
