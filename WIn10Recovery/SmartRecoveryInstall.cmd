d0a7dd855f5cd4ef18e4567502eb49b622d892df
markatgigabyte <mark@gigabyte.com>
2015-10-09 01:19:35 +0800

    Update SmartRecoveryInstall.cmd

--- WIn10Recovery/SmartRecoveryInstall.cmd
+++ WIn10Recovery/SmartRecoveryInstall.cmd
@@ -5,16 +5,23 @@ set TARGETOSMODE="UEFI"
 set USBDISKVOL=X:
 
 
-rem Step1: Create RecoveryImage Partition
+rem Create RecoveryImage Partition
 batch\BatchSubstitute.bat "$TARGETDISK$" %TARGETDISK% CreateRecoveryPartitions-%TARGETOSMODE%.txt > CreateRecoveryPartitions.scp
 batch\BatchSubstitute.bat "$RECOVERYPARTIONSIZE$" %RECOVERYPARTIONSIZE% CreateRecoveryPartitions.scp > CreateRecoveryPartitions.scp
 diskpart /s CreateRecoveryPartitions.scp
+
+
 mkdir r:\RecoveryImage
 copy %USBDISKVOL%\RecoveryImage\Install.wim r:\RecoveryImage
 
-
+rem Add a Script to Push-Button Reset Features
 AddOEMResetBtnSupport.cmd
+rem Add a Hardware Recovery Button to Start Windows RE
 AddHWRecoveryBtnSupport.cmd
+rem Add a Custom Tool to the Windows RE Boot Options Menu
 AddCustomToolBtnSupport.cmd
 
+rem Hide Recovery & WinRE Partition
+batch\BatchSubstitute.bat "$TARGETDISK$" %TARGETDISK% HideRecoveryPartitions-%TARGETOSMODE%.txt > HideRecoveryPartitions.scp
+diskpart /s HideRecoveryPartitions.scp
 
