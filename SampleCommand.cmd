22587fc81bbef079dac4026c663a5da0c33c17c4
Mark Tsai <marktsai0316@gmail.com>
2015-10-08 13:25:13 +0800

    Delete SampleCommand.cmd

--- SampleCommand.cmd
+++
@@ -1,3 +0,0 @@
-rem This script creates a system report by using the DirectX Diagnostic tool, and sends it to a new folder called C:\Fabrikam.
-md C:\Fabrikam
-C:\Windows\System32\dxdiag /t C:\Fabrikam\DxDiag-TestLogFiles.txt
